2d9079cfd7243ed1551736370aab869eb9008513
TheMagicalAubster <aubrey.blairpattison.gmail.com>
2020-04-18 17:43:19 +0000

    Testing add after removing directory

---
+++ conditionals.js
@@ -0,0 +1,23 @@
+const raining = true;
+//const cold = false; 
+
+const temperature = 12; 
+
+if (raining){
+console.log("Don't forget your umbrella!");
+}
+
+if (temperature < 0) {
+console.log("Make sure you pick out a scarf!");
+}
+else if (temperature < 15) {
+console.log("You'll want long sleeves today.");
+}
+
+else {
+console.log("Short sleeves it is!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
